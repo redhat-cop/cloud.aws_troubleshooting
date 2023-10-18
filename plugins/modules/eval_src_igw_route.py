7f9816784a91d7b17fdfedec60deef0456fd159d
abikouo <abikouo@redhat.com>
2023-10-18 18:10:34 +0200

    Pin ansible-lint to specific version to avoid recurrent CI failures with new releases of ansible-lint action

--- plugins/modules/eval_src_igw_route.py
+++ plugins/modules/eval_src_igw_route.py
@@ -72,101 +72,101 @@ EXAMPLES = r"""
     # Network ACL entries order
     # ["rule_number", "protocol", "rule_action", "cidr_block", "icmp_type", "icmp_code", "port_from", "port_to"]
     src_network_acls:
-        - egress:
-            - - 100
-              - "all"
-              - "allow"
-              - "0.0.0.0/0"
-              - null
-              - null
-              - 0
-              - 65535
-        - ingress:
-            - - 100
-              - "all"
-              - "allow"
-              - "0.0.0.0/0"
-              - null
-              - null
-              - 0
-              - 65535
+      - egress:
+          - - 100
+            - "all"
+            - "allow"
+            - "0.0.0.0/0"
+            - null
+            - null
+            - 0
+            - 65535
+      - ingress:
+          - - 100
+            - "all"
+            - "allow"
+            - "0.0.0.0/0"
+            - null
+            - null
+            - 0
+            - 65535
     src_network_interface:
-        association:
-            ip_owner_id: "amazon"
-            public_dns_name: "ec2-3-93-192-138.compute-1.amazonaws.com"
-            public_ip: "3.93.192.138"
-        attachment:
-            attach_time: "2022-08-29T12:49:23+00:00"
-            attachment_id: "eni-attach-01c6b11b7ea6d15da"
-            delete_on_termination: true
-            device_index: 0
-            instance_id: "i-093745484bef0531c"
-            instance_owner_id: "721066863947"
-            network_card_index: 0
-            status: "attached"
-        availability_zone: "us-east-1a"
-        description: ""
-        groups:
-            - group_id: "sg-030921b67798a88fd"
-              group_name: "sg_ansibleVPC_publicsubnet_jumphost"
-        id: "eni-046bc4b5d5249b832"
-        interface_type: "interface"
-        ipv6_addresses: []
-        mac_address: "0e:32:64:a5:39:f7"
-        network_interface_id: "eni-046bc4b5d5249b832"
-        owner_id: "721066863947"
-        private_dns_name: "ip-192-168-0-11.ec2.internal"
-        private_ip_address: "192.168.0.11"
-        private_ip_addresses:
-            - association":
-                ip_owner_id: "amazon"
-                public_dns_name: "ec2-3-93-192-138.compute-1.amazonaws.com"
-                public_ip: "3.93.192.138"
-              primary: true
-              private_dns_name: "ip-192-168-0-11.ec2.internal"
-              private_ip_address: "192.168.0.11"
-        requester_managed: false
-        source_dest_check: true
-        status: "in-use"
-        subnet_id: "subnet-03181dc286eca1244"
-        tag_set: {}
-        tags: {}
-        vpc_id: "vpc-02c18b2cb55d28ff3"
-    src_port_range: null
-    src_security_groups_info:
-        - description: "Allow 22 and 80 ports open"
-          group_id: "sg-0258afe8541042bac"
+      association:
+        ip_owner_id: "amazon"
+        public_dns_name: "ec2-3-93-192-138.compute-1.amazonaws.com"
+        public_ip: "3.93.192.138"
+      attachment:
+        attach_time: "2022-08-29T12:49:23+00:00"
+        attachment_id: "eni-attach-01c6b11b7ea6d15da"
+        delete_on_termination: true
+        device_index: 0
+        instance_id: "i-093745484bef0531c"
+        instance_owner_id: "721066863947"
+        network_card_index: 0
+        status: "attached"
+      availability_zone: "us-east-1a"
+      description: ""
+      groups:
+        - group_id: "sg-030921b67798a88fd"
           group_name: "sg_ansibleVPC_publicsubnet_jumphost"
-          ip_permissions:
-            - from_port: 80
-              ip_protocol: "tcp"
-              ip_ranges:
-                - cidr_ip: "223.230.126.232/32"
-                  description: "Allow 22 and 80 from all"
-              ipv6_ranges: []
-              prefix_list_ids: []
-              to_port: 80
-              user_id_group_pairs: []
-            - from_port: 22
-              ip_protocol: "tcp"
-              ip_ranges:
-                - cidr_ip: "192.168.0.0/24"
-                - cidr_ip: "223.230.126.232/32"
-                  description: "Allow 22 and 80 from all"
-              ipv6_ranges: []
-              prefix_list_ids: []
-              to_port: 22
-              user_id_group_pairs: []
-          ip_permissions_egress:
-            - ip_protocol: "-1"
-              ip_ranges":
-                - cidr_ip: "0.0.0.0/0"
-              ipv6_ranges: []
-              prefix_list_ids: []
-              user_id_group_pairs: []
-          owner_id: "721066863947"
-          tags: {}
-          vpc_id: "vpc-097bb89457aa6d8f3"
+      id: "eni-046bc4b5d5249b832"
+      interface_type: "interface"
+      ipv6_addresses: []
+      mac_address: "0e:32:64:a5:39:f7"
+      network_interface_id: "eni-046bc4b5d5249b832"
+      owner_id: "721066863947"
+      private_dns_name: "ip-192-168-0-11.ec2.internal"
+      private_ip_address: "192.168.0.11"
+      private_ip_addresses:
+        - association":
+          ip_owner_id: "amazon"
+          public_dns_name: "ec2-3-93-192-138.compute-1.amazonaws.com"
+          public_ip: "3.93.192.138"
+          primary: true
+          private_dns_name: "ip-192-168-0-11.ec2.internal"
+          private_ip_address: "192.168.0.11"
+      requester_managed: false
+      source_dest_check: true
+      status: "in-use"
+      subnet_id: "subnet-03181dc286eca1244"
+      tag_set: {}
+      tags: {}
+      vpc_id: "vpc-02c18b2cb55d28ff3"
+      src_port_range: null
+    src_security_groups_info:
+      - description: "Allow 22 and 80 ports open"
+        group_id: "sg-0258afe8541042bac"
+        group_name: "sg_ansibleVPC_publicsubnet_jumphost"
+        ip_permissions:
+          - from_port: 80
+            ip_protocol: "tcp"
+            ip_ranges:
+              - cidr_ip: "223.230.126.232/32"
+                description: "Allow 22 and 80 from all"
+            ipv6_ranges: []
+            prefix_list_ids: []
+            to_port: 80
+            user_id_group_pairs: []
+          - from_port: 22
+            ip_protocol: "tcp"
+            ip_ranges:
+              - cidr_ip: "192.168.0.0/24"
+              - cidr_ip: "223.230.126.232/32"
+                description: "Allow 22 and 80 from all"
+            ipv6_ranges: []
+            prefix_list_ids: []
+            to_port: 22
+            user_id_group_pairs: []
+        ip_permissions_egress:
+          - ip_protocol: "-1"
+            ip_ranges":
+              - cidr_ip: "0.0.0.0/0"
+            ipv6_ranges: []
+            prefix_list_ids: []
+            user_id_group_pairs: []
+        owner_id: "721066863947"
+        tags: {}
+        vpc_id: "vpc-097bb89457aa6d8f3"
 """
 
 
